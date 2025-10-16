8397b248374c0669d8bab666d5b8b354b4a3bcc6
UITxWoodyNguyen <woodydadrillers0707@gmail.com>
2025-10-16 22:09:58 +0700

    Create no-template.cpp

---
+++ no-template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = a, _b = (b); i <= _b; ++i)
+#define REP(i, a, b) for (int i = a, _b = (b); i < _b; ++i)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; --i)
+#define nl cout << endl
+#define endl '\n'
+
+#define JUDGE(name) if (fopen(name".inp", "r")) { freopen(name".inp","r",stdin); freopen(name".out","w",stdout); }
+
+void solve() {
+    
+}
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    JUDGE("filename");
+    
+    solve();
+}
